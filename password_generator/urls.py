f666982b6a7646db2ebc3f666d83f9e77dba9269
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-07-08 22:57:37 +0600

    2nd Commit

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path
 
+from django.urls import path
+from generator import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', generator.urls, name=''),
+
+    path('',views.home , name='home'),
 ]
