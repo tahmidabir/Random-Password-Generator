2768b1ad29cd781e9b7502871790e95a5efef138
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-07-10 23:20:37 +0600

    Generated Random password generator of 10 length for characters only

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
@@ -7,5 +8,13 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'generator/home.html')
 
+
 def password(request):
-    return render(request,'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = 10
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request,'generator/password.html',{'password':thepassword})
